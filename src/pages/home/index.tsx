80c9d974019a1e9bc81ee8ffc011687ddfd845dc
deverick <dncellassistec@gmail.com>
2025-04-01 17:32:19 -0300

    criado componente container e impute e criamos paginas login e register

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,50 @@
+import { Container } from "../../components/container";
 
 export function Home() {
-    return (
-      <div>
-        <h1>Pagina Home</h1>
-      </div>
-    )
-  }
-  
-  
+  return (
+    <Container>
+      <section className="bg-white p-4 rounded-lg w-full max-w-3xl mx-auto flex justify-center items-center gap-2">
+        <input
+          type="text"
+          className="w-full border-2 rounded-lg h-9 px-3 border-gray-400 outline-none"
+          placeholder="Digite o nome do carro..."
+        />
+        <button className="bg-red-500 h-9 px-8 rounded-lg text-white font-medium text-lg">
+          Buscar
+        </button>
+      </section>
+
+      <h1 className=" text-center flex justify-center items-center text-2xl font-bold mt-6 mb-4">
+        Carros novos e usados em todo o Brasil
+      </h1>
+
+      <main className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+
+        <section className="w-full bg-white rounded-lg">
+          <img
+            className="w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all"
+            src="https://image.webmotors.com.br/_fotos/anunciousados/gigante/2025/202501/20250107/volkswagen-jetta-1.4-250-tsi-total-flex-rline-tiptronic-wmimagem16300887596.jpg?s=fill&w=552&h=414&q=60"
+            alt="Carro"
+          />
+          <p className="font-bold mt-1 mb-2 px-2">Volkswagen Jetta</p>
+
+          <div className="flex flex-col px-2">
+            <span className="text-zinc-700 mb-6">
+              Ano 2023/2023 | 50.000 Km
+            </span>
+            <strong className="text-black font-medium text-xl">
+              R$ 102.000
+            </strong>
+          </div>
+
+          <div className="w-full h-px bg-slate-300 my-2"></div>
+
+          <div className="px-2 pb-2">
+            <span className="text-zinc-900 ">Paranaíba MS</span>
+          </div>
+        </section>
+
+      </main>
+    </Container>
+  );
+}
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,10 +1,75 @@
+import logoImg from "../../assets/logo.svg";
+import { Container } from "../../components/container";
+import { Link } from "react-router-dom";
+import { Input } from "../../components/input";
+
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  email: z
+    .string()
+    .email("Insira um email válido")
+    .nonempty("O campo email é obrigatório"),
+  password: z.string().nonempty("O campo senha é obrigatório"),
+});
+
+type FormData = z.infer<typeof schema>;
 
 export function Login() {
-    return (
-      <div>
-        <h1>Pagina login</h1>
-      </div>
-    )
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+    mode: "onChange",
+  });
+
+  function onsubmit(data: FormData) {
+    console.log(data);
   }
-  
-  
+
+  return (
+    <Container>
+      <div className="w-full min-h-screen flex justify-center items-center flex-col gap-4">
+        <Link className="mb-6 max-w-sm w-full" to="/">
+          <img src={logoImg} alt="Logo do site" className="w-full" />
+        </Link>
+
+        <form
+          className="bg-white max-w-xl w-full rounded-lg p-4"
+          onSubmit={handleSubmit(onsubmit)}
+        >
+          <div className="mb-3 ">
+            <Input
+              type="email"
+              placeholder="Digite seu email..."
+              name="email"
+              error={errors.email?.message}
+              register={register}
+            />
+          </div>
+
+          <div className="mb-3 ">
+            <Input
+              type="password"
+              placeholder="Digite sua senha..."
+              name="password"
+              error={errors.password?.message}
+              register={register}
+            />
+          </div>
+
+          <button type="submit" className="bg-zinc-900 w-full rounded-md text-white h-10 font-medium">Acessar</button>
+        </form>
+
+        <Link to="/register">
+        <p className="hover:text-blue-700">Ainda não possui uma conta? Cadastre-se</p>
+        </Link>
+
+      </div>
+    </Container>
+  );
+}
